002adb77150617609b3bb4a873960695d7ed315a
DanielFonseca117 <danifon44@gmail.com>
2025-08-23 12:19:54 -0500

    Se cambiaron cosas, del usuario, pero quede atrasado en el action, toca adelantar

---
+++ sistema/resources/views/usuario/index.blade.php
@@ -0,0 +1,59 @@
+@extends('plantilla.app')
+@section('contenido')
+<div class="app-content">
+    <!--begin::Container-->
+    <div class="container-fluid">
+        <!--begin::Row-->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h3 class="card-title">Usuarios</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <div class="table-responsive mt-3">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th style="width: 5rem">ID</th>
+                                    <th style="width: 5rem">Nombre</th>
+                                    <th style="width: 5rem">Correo</th>
+                                    <th style="width: 5rem">Opciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>1</th>
+                                    <th>Daniel Fonseca</th>
+                                    <th>danifon44@gmail.com</th>
+                                    <td><a href="#" class="btn btn-warning btn-sm">
+                                            <i class="bi bi-pencil-fill"></i></a>
+                                        <button class="btn btn-danger btn-sm">
+                                            <i class="bi bi-trash-fill"></i></button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer clearfix">
+                        <ul class="pagination pagination-sm m-0 float-end">
+                            <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /.card -->
+                <!-- /.col -->
+            </div>
+            <!--end::Row-->
+        </div>
+        <!--end::Container-->
+    </div>
+
+    @endsection
